347052b1cdd5cbc6ecece18568e3c69ac5cb6e21
weirdociao <1940005020@qq.com>
2023-07-30 00:40:25 +0800

    v1.2

--- src/main/java/com/Blog/common/Result.java
+++ src/main/java/com/Blog/common/Result.java
@@ -1,13 +1,20 @@
 package com.Blog.common;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
 
 @Data
 public class Result<T> implements Serializable {
+
+    @ApiModelProperty(value = "响应编码:200-请求处理成功，400-失败")
     private Integer code;//状态码 200代表成功
+
+    @ApiModelProperty(value = "响应消息")
     private String msg;//返回消息信息
+
+    @ApiModelProperty(value = "响应数据")
     private T data;//返回数据
 
     public static <T> Result<T> success(T data) {//成功
