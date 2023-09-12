804050495a24d4b792cc43e986a392b827ed043f
weirdociao <1940005020@qq.com>
2023-09-12 12:04:47 +0800

    v1.5

--- src/main/java/com/Blog/strategy/EmailLoginStrategy.java
+++ src/main/java/com/Blog/strategy/EmailLoginStrategy.java
@@ -1,9 +1,12 @@
 package com.Blog.strategy;
 
+import com.Blog.common.CustomException;
 import com.Blog.constants.LoginStrategyConstant;
 import com.Blog.model.dto.EmailLoginReq;
 import com.Blog.strategy.core.AbstractExecuteStrategy;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
@@ -17,6 +20,7 @@ public class EmailLoginStrategy implements AbstractExecuteStrategy<EmailLoginReq
 
     private final HttpServletResponse response;
 
+    private final StringRedisTemplate redisTemplate;
     @Override
     public String mark() {
         return LoginStrategyConstant.EMAIL_LOGIN_MARK;
@@ -26,7 +30,11 @@ public class EmailLoginStrategy implements AbstractExecuteStrategy<EmailLoginReq
     public Boolean executeResp(EmailLoginReq requestParam) {
 
         //TODO 将验证码取出，与Redis中的进行对比，一致则登录成功，删除Redis验证码
-
+        String value = redisTemplate.opsForValue().get(requestParam.getEmail() + "_code");
+        if (StringUtils.equalsIgnoreCase(value,requestParam.getEmailCode())){
+            throw new CustomException("验证码错误");
+        }
+        redisTemplate.delete(requestParam.getEmail() + "_code");
 
         return AbstractExecuteStrategy.super.executeResp(requestParam);
     }
